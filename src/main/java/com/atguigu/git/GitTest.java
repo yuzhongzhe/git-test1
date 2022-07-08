04eba9ace51ced767bb7b076ca899549b03ee699
zzyu <zhongzhe_yu@163.com>
2022-07-09 00:48:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * author:
+ * Date:2022/7/90:42
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
