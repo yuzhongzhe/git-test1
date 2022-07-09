f2b35093f6435a0c0d8de763098bda0374977c56
zzyu <zhongzhe_yu@163.com>
2022-07-09 12:21:53 +0800

    master commit4

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,7 +12,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("master test!");
-        System.out.println("我是master 不能动!");
     }
 
 }
