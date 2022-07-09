292c6d1113dceac774b3325251a76a5b8c03cabe
zzyu <zhongzhe_yu@163.com>
2022-07-09 12:39:11 +0800

    master commit7

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("hello git5!");
         System.out.println("======hot-fix-test!======");
+        System.out.println("22222222222======master-test!======222222222222");
     }
 
 }
